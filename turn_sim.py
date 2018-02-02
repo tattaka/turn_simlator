e73d7970460c354c681e1f0bf8e451d01c024c84
tattaka <taka_aki@me.com>
2018-02-02 14:20:30 +0900

    ガワだけ作った

--- turn_sim.py
+++ turn_sim.py
@@ -19,6 +19,12 @@ class Window(QWidget):
     def initUI(self):
         self.setWindowTitle('Turn Simulator')
 
+        self.graphicsView = QGraphicsView()
+        scene = QGraphicsScene(self.graphicsView)
+        scene.setSceneRect(0, 0, 500, 500)
+        self.graphicsView.setScene(scene)
+
+
         self.plot_button = QPushButton("start", self)
         self.plot_button.setCheckable(True)
         self.plot_button.toggled.connect(self.slot_plot_button_toggled)
@@ -29,23 +35,49 @@ class Window(QWidget):
         self.save_button = QPushButton("save", self)
         self.save_button.clicked.connect(self.slot_save_button_pushed)
 
-        self.label1 = QLabel("test1")
-        self.label2 = QLabel("test2")
+        validator = QIntValidator(0,100)
+        self.paramEdit1 = QLineEdit()
+        self.paramEdit1.setValidator(validator)
+        param1layout = QHBoxLayout()
+        param1layout.addWidget(QLabel("param1:"))
+        param1layout.addWidget(self.paramEdit1)
+        param1layout.addWidget(QLabel("[mm]"))
+
+        self.paramEdit2 = QLineEdit()
+        self.paramEdit1.setValidator(validator)
+        param2layout = QHBoxLayout()
+        param2layout.addWidget(QLabel("param2:"))
+        param2layout.addWidget(self.paramEdit2)
+        param2layout.addWidget(QLabel("[mm]"))
+
+        self.combo = QComboBox(self)
+        self.combo.addItem("-- select --")
+        self.combo.addItem("90(search)")
+        self.combo.addItem("45")
+        self.combo.addItem("90(short)")
+        self.combo.addItem("135")
+        self.combo.addItem("180")
+        self.combo.addItem("90(slanting)")
 
         layout2 = QHBoxLayout()
         layout2.addWidget(self.plot_button)
         layout2.addWidget(self.reset_button)
         layout2.addWidget(self.save_button)
 
-        layout3 = QHBoxLayout()
-        layout3.addWidget(self.label1)
-        layout3.addWidget(self.label2)
+        layout3 = QVBoxLayout()
+        layout3.addWidget(self.combo)
+        layout3.addLayout(param1layout)
+        layout3.addLayout(param2layout)
 
-        layout = QVBoxLayout()
+        layout1 = QVBoxLayout()
+        layout1.addWidget(self.graphicsView)
 
-        layout.addLayout(layout3)
-        layout.addLayout(layout2)
-        self.setLayout(layout)
+        mainlayout = QHBoxLayout()
+
+        layout1.addLayout(layout2)
+        mainlayout.addLayout(layout1)
+        mainlayout.addLayout(layout3)
+        self.setLayout(mainlayout)
 
     def slot_plot_button_toggled(self, checked):
         if checked:
